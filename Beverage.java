b0fd07fe88ce2057221d93396effed4caf76cf99
PhoenixLenny <lenny.thuo@strathmore.edu>
2025-08-27 12:44:34 +0300

    Update Beverage.java

--- Beverage.java
+++ Beverage.java
@@ -2,20 +2,17 @@ public class Beverage extends MenuItem {
     private String size;
     private boolean isHot;
 
-    // Constructor
     public Beverage(int itemId, String name, double price, String description, String size, boolean isHot) {
         super(itemId, name, price, description);
         this.size = size;
         this.isHot = isHot;
     }
 
-    // Getters and Setters
     public String getSize() { return size; }
     public void setSize(String size) { this.size = size; }
     public boolean isHot() { return isHot; }
     public void setHot(boolean hot) { isHot = hot; }
 
-    // Polymorphism: Override
     @Override
     public String getDetails() {
         return "Beverage: " + getName() + " (" + size + ", Hot: " + isHot + ") - $" + getPrice();
@@ -24,4 +21,5 @@ public class Beverage extends MenuItem {
     public void serve() {
         System.out.println("Serving " + getName() + " in " + size + " size.");
     }
+
 }
